820aca555d0fb22556d45356c2a02a6b26ad0465
santiago <santiago.cocco1@gmail.com>
2024-02-26 01:35:11 -0300

    mi primer comit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://santi-cocco.github.io/https://github.com/santi-cocco/primer-pre-entrega-React.git'
+  base: '/'
 })
